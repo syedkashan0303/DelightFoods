b54aba4f1438cd5240eae51326c454c20a1079e7
syedkashan0303 <sayedkashan58@gmail>
2024-03-29 01:43:25 +0500

    changes

--- DelightFoods_Live/Controllers/CartController.cs
+++ DelightFoods_Live/Controllers/CartController.cs
@@ -39,6 +39,29 @@ namespace DelightFoods_Live.Controllers
         }
 
         [HttpPost]
+        public JsonResult AddItemCart(CartModel model)
+        {
+            if (model.ProductId > 0)
+            {
+                var cart = _context.Cart.FirstOrDefault(x => x.ProductId == model.ProductId && x.CustomerId == model.CustomerId);
+
+                if (cart != null )
+                {
+                    cart.Quantity = cart.Quantity + 1;
+                    _context.Update(cart);
+                }
+                else
+                {
+                    _context.Add(model);
+                }
+                _context.SaveChangesAsync();
+
+                return Json("succes");
+            }
+            return Json("error");
+        }
+
+        [HttpPost]
         public JsonResult RemoveItemFromCart(int id)
         {
             if (id > 0)
@@ -60,6 +83,24 @@ namespace DelightFoods_Live.Controllers
             return Json("error");
         }
 
+        [HttpPost]
+        public JsonResult RemoveAllItemFromCart(int id)
+        {
+            if (id > 0)
+            {
+                var cart = _context.Cart.FirstOrDefault(m => m.Id == id);
+
+                if (cart == null)
+                {
+                    return Json("error");
+                }
+                _context.Remove(cart);
+                _context.SaveChangesAsync();
+
+                return Json("succes");
+            }
+            return Json("error");
+        }
 
     }
 }
